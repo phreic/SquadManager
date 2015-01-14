0ae324d917df1eefbb45d2a451f5e4d61383051e
LumPenPacK <prei@me.com>
2015-01-14 18:00:18 +0100

    Update SquadManager.cs

--- SquadManager.cs
+++ SquadManager.cs
@@ -2889,12 +2889,10 @@ This means if you disable a feature or change a setting the chat message will be
 
                 if (soldierName == Inviter)
                 {
-                    if (squadId == 0)
-                    {
+                 
                         RemoveJoinSwitch(soldierName);
                         return;
-                    }
-                }
+                             }
             }
 
 
